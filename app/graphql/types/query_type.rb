aa6cabbd7589e618d70ce8afef02693b13ba9d47
lamach1n3 <taillefer.jf@gmail.com>
2021-03-16 06:51:12 -0400

    not working properly

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,14 +4,12 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :all_links, [LinkType], null: false
+    def all_links
+      Link.all
     end
   end
 end
