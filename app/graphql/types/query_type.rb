175a90fc545eb69d10bf2122432c8c10f0a30324
lamach1n3 <taillefer.jf@gmail.com>
2021-03-17 10:09:03 -0400

    found the good app got connection to

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -11,5 +11,30 @@ module Types
     def all_links
       Link.all
     end
+    field :employees, [as], null: false
+    def all_links
+      Link.all
+    end
   end
 end
+
+
+# EXAMPLE START #
+
+    # /users
+    # field :users, [Types::UserType], null: false
+    # 
+    # def users
+    #   User.all
+    # end
+    
+    # /user(id:)
+    # field :user, Types::UserType, null: false do
+    #   argument :id, ID, required: true
+    # end
+    # 
+    # def user(id:)
+    #   User.find(id)
+    # end
+
+    # EXAMPLE END #
